be5a018caa63d704330fdd948ecfcbe51131ac1a
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-07-02 13:37:48 +0300

    DOM

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-var elBox = document.getElementById("box");
+var elBox = document.getElementsByClassName("myBox"); // seleciona o primeiro elemento com a classe "myBox"
 elBox.style.width = "300px";
-elBox.style.height = "300px";
-elBox.style.backgroundColor = "red";
+elBox.style.height = "200px";
+elBox.style.backgroundColor = "red"; // define a cor de fundo como vermelho
+
+for (i = 0; i < elBox.length, i++; ) {
+  var el = elBox[i];
+
+  el.style.width = "300px";
+  el.style.height = "200px";
+  el.style.backgroundColor = "red"; // define a cor de fundo como vermelho
+}
