cc356f8029c4f7add218e7330b65834e96da21fc
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-07-04 07:39:45 +0300

    DOM

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var conteudo = document
-    .querySelector(".conteudo h2 span")
-    .addEventListener("click", function () {
-      alert("olá");
-    });
-});
+var conteudo = document
+  .querySelector(".conteudo h2 span")
+  .addEventListener("click", () => {
+    alert("olá");
+  });
