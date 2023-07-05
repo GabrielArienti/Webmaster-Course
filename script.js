6e2fbc6596a1e61c78c2b33cbdb9309fb1b0312c
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-07-05 07:00:21 +0300

    Modern Javascript

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-var conteudo = document
-  .querySelector(".conteudo h2 span")
-  .addEventListener("click", function () {
-    var paragrafos = document.querySelectorAll(".conteudo p");
-    paragrafos.forEach(function (paragrafo) {
-      if (paragrafo.classList.contains("mostrar")) {
-        paragrafo.classList.remove("mostrar");
-        paragrafo.classList.add("ocultar");
-      } else {
-        paragrafo.classList.remove("ocultar");
-        paragrafo.classList.add("mostrar");
-      }
-    });
-  });
+nomes = (...nomes) => {
+  console.log(nomes);
+};
+
+console.log(nomes(1, 2, 3, 4, 5, 6, 7, 8, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
