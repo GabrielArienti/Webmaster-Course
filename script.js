9c83c37fb82f6aabcf28265a2d10e40dc5c39271
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-07-06 07:46:37 +0300

    Fecth

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-setTimeout(function () {
-  alert("Iniciou");
-  var request = new XMLHttpRequest();
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
-
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      // sucesso
-      var data = JSON.parse(request.responseText);
-      console.log(data);
-    } else {
-      // erro
-    }
-  };
-
-  request.onerror = function () {
-    // erro de rede
-  };
-
-  request.send();
-}, 5000);
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
