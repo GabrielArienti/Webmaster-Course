909d4dce9b46162ef72dfa0442136d6997dd3366
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-07-06 07:26:34 +0300

    AJAX

---
+++ AJAX/script.js
@@ -0,0 +1,18 @@
+var request = new XMLHttpRequest();
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+
+request.onload = function () {
+  if (request.status >= 200 && request.status < 400) {
+    // sucesso
+    var data = JSON.parse(request.responseText);
+    console.log(data);
+  } else {
+    // erro
+  }
+};
+
+request.onerror = function () {
+  // erro de rede
+};
+
+request.send();
--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-var request = new XMLHttpRequest();
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+setTimeout(function () {
+  alert("Iniciou");
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
 
-request.onload = function () {
-  if (request.status >= 200 && request.status < 400) {
-    // sucesso
-    var data = JSON.parse(request.responseText);
-    console.log(data);
-  } else {
-    // erro
-  }
-};
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // sucesso
+      var data = JSON.parse(request.responseText);
+      console.log(data);
+    } else {
+      // erro
+    }
+  };
 
-request.onerror = function () {
-  // erro de rede
-};
+  request.onerror = function () {
+    // erro de rede
+  };
 
-request.send();
+  request.send();
+}, 5000);
