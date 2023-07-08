25f18df782965c92ac8614930a4b16fedcdff380
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-07-08 12:02:03 +0300

    GAme

---
+++ Rock, paper and scissors/script.js
@@ -0,0 +1,16 @@
+var elements = document.querySelectorAll(".player-options div > img");
+
+// function to reset opacity.
+function resetOpacityPLayer() {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.opacity = 0.3;
+  }
+}
+
+// selection of opcion (player)
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function () {
+    resetOpacityPLayer();
+    this.style.opacity = 1;
+  });
+}
