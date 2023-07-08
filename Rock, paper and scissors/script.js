ad7ac42439ee57dad8052050813331f1e6b78f61
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-07-08 13:51:20 +0300

    Game

--- Rock, paper and scissors/script.js
+++ Rock, paper and scissors/script.js
@@ -2,6 +2,34 @@ var elements = document.querySelectorAll(".player-options div > img");
 
 //
 var playerOpt = "";
+var inimigoOpt = "";
+
+// resert for enemy
+function resetInimigo() {
+  const enemyOptions = document.querySelectorAll(".enemy-options div");
+  for (var i = 0; i < enemyOptions.length; i++) {
+    enemyOptions[i].childNodes[0].style.opacity = 0.3;
+  }
+}
+
+// function for the enemy
+function inimigoJogar() {
+  let rand = Math.floor(Math.random() * 3);
+
+  const enemyOptions = document.querySelectorAll(".enemy-options div");
+  resetInimigo();
+
+  for (var i = 0; i < enemyOptions.length; i++) {
+    if (i == rand) {
+      enemyOptions[i].childNodes[0].style.opacity = 1;
+      inimigoOpt = enemyOptions[i].childNodes[0].getAttribute("opt");
+    }
+  }
+
+  alert(playerOpt);
+  alert(inimigoOpt);
+}
+
 // function to reset opacity.
 function resetOpacityPLayer() {
   for (var i = 0; i < elements.length; i++) {
@@ -15,6 +43,8 @@ for (var i = 0; i < elements.length; i++) {
     resetOpacityPLayer();
     this.style.opacity = 1;
     playerOpt = this.getAttribute("opt");
-    alert(playerOpt);
+
+    // enemy round
+    inimigoJogar();
   });
 }
