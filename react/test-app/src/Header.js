68c533746ebd571a1c968a613ebe5758a4bcdac0
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-07-13 09:07:04 +0300

    React app

---
+++ react/test-app/src/Header.js
@@ -0,0 +1,11 @@
+// import './Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <p>Lorem ipsum dolore sit amet. </p>
+    </div>
+  );
+}
+
+export default Header;
